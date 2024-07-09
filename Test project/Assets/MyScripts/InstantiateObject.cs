115be022bc8962e4663d69a35ba351e2483bb16c
MHoupli <maelhoupline@gmail.com>
2024-07-09 11:24:12 +0200

    Move directory

---
+++ Test project/Assets/MyScripts/InstantiateObject.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class InstantiateObject : MonoBehaviour
+{
+    public void InstantiateNewObject(GameObject go)
+    {
+        //Instantiate(go, button.transform.position, button.transform.rotation);
+        Debug.Log("Creating");
+    }
+}
